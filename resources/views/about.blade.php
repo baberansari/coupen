70e87816db6036a80a839ed3f10cc64f7bae6849
HP 450 G7 <babaeransari21@gmail.com>
2025-03-25 05:21:29 +0500

    added page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.site.site')
+
+@section('content')
+
+<section class="top-coupon-sec product-tiles">
+    <div class="container">
+        <div class="sec-title">
+            <h3>Trending Coupon</h3>
+        </div>
+        <div class="row">
+
+
+
+
+        </div>
+    </div>
+</section>
+@endsection
