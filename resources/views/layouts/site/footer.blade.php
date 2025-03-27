a7d8234510748cdf168d1ec45a69f503f0715eb7
Daniel Khan <dk@danielkhan.co>
2025-03-27 11:44:49 -0400

    modify footer

--- resources/views/layouts/site/footer.blade.php
+++ resources/views/layouts/site/footer.blade.php
@@ -131,7 +131,7 @@ $setting = settings();
                                 gap: 26px;">
 
                         <a href="https://www.facebook.com/RetailMeNot" aria-label="RetailMeNot on Facebook">
-                            <svg class="mx-auto mt-2 h-6 w-6" fill="currentColor" viewBox="0 0 320 512"><path d="m279.14 288 14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg>                    </a>
+                            <svg style="    width: 17px;" class="mx-auto mt-2 h-6 w-6" fill="currentColor" viewBox="0 0 320 512"><path d="m279.14 288 14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg>                    </a>
                         <a href="https://www.instagram.com/retailmenot" aria-label="RetailMeNot on Instagram">
                             <svg class="mx-auto mt-2 h-6 w-6" fill="currentColor" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141m0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7m146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8m76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8M398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1"></path></svg>                    </a>
                         <a href="https://www.pinterest.com/retailmenot" aria-label="RetailMeNot on Pinterest">
@@ -143,14 +143,6 @@ $setting = settings();
                 </div>
                 <div class="grid_8">
                     <div class="flex flex-wrap gap-4 font-proxima lg:gap-6 [&amp;>a]:block">
-                        <a class="underline-offset-4 hover:underline" href="/ca/">RetailMeNot Canada</a>
-                        <button class="h-fit cursor-pointer underline-offset-4 hover:underline" onclick="window.zdconsent.showConsentTool(event);"><img class="mr-1 inline" src="https://cdn.ziffstatic.com/pub/icong1.png" alt="AdChoices Icon" loading="lazy" width="8" height="8">
-                            AdChoices</button>
-                        <a class="underline-offset-4 hover:underline" href="https://www.ziffdavis.com/terms-of-use">Terms of Service</a>
-                        <a class="underline-offset-4 hover:underline" href="https://www.ziffdavis.com/shopping-privacy-policy">Privacy Policy</a>
-                        <a class="underline-offset-4 hover:underline" href="https://dsar.retailmenot.com/">Do Not Sell My Personal Information</a>
-                        <a class="underline-offset-4 hover:underline" href="https://www.ziffdavis.com/accessibility">Accessibility</a>
-                        <a class="underline-offset-4 hover:underline" href="/sitemap/">Sitemap</a>
                     </div>
                 </div>
                 {{-- <div class="left-link">
